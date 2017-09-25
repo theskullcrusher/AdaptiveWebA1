8048d411b5257771fe454c4eb2d581c31c6054e0
Suraj Shah <ssshah22@asu.edu>
2017-09-25 00:37:33 +0000

    assignment 2 starts

--- install_packages.sh
+++ install_packages.sh
@@ -4,3 +4,8 @@ sudo apt-get install python-pip
 sudo apt-get install gunicorn supervisor nginx
 sudo apt-get install build-essential libssl-dev libffi-dev
 sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs
+sudo npm install -g bower
+
