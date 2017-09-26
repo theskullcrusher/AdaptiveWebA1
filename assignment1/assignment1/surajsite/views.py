e3fa6f36118da9e0f5e1304a1555eb8173e27a76
Suraj Shah <ssshah22@asu.edu>
2017-09-25 22:54:07 -0700

    fusioncharts

--- assignment1/assignment1/surajsite/views.py
+++ assignment1/assignment1/surajsite/views.py
@@ -102,169 +102,164 @@ def analytics(request):
 		Analytics for user
 	"""
 	try:
-		user = request.user.username
-		usr = SiteUser.objects.filter(username=user)[0]
-		objs = UserLogs.objects.filter(user=usr).values('action').annotate(total=Count('action')).order_by('total')
-		xdata = []
-		ydata = []
-		for obj in objs:
-			xdata.append(str(obj['action']))
-			ydata.append(str(obj['total']))		
-		#print str(xdata)
-		#print str(ydata)
-		color_list = ['#5d8aa8', '#e32636', '#efdecd', '#ffbf00', '#ff033e', '#a4c639',
-                '#b2beb5', '#8db600', '#7fffd4', '#ff007f', '#ff55a3', '#5f9ea0']
-		extra_serie = {
-        	"tooltip": {"y_start": " event called ", "y_end": " times"},
-        	"color_list": color_list
-    		}
-		chartdata = {'x': xdata, 'y1': ydata, 'extra1': extra_serie}
-		charttype = "pieChart"
-		data = {
-			'charttype': charttype,
-			'chartdata': chartdata,
-			'chartcontainer': "piechart_container",
-			'extra': {
-			'x_is_date': False,
-			'x_axis_format': '',
-			'tag_script_js': True,
-			'jquery_on_ready': False,
-			}
-			}
+		# tab = request.GET.get('tab', '1')
+		# if tab == '1':
+			user = request.user.username
+			usr = SiteUser.objects.filter(username=user)[0]
+			objs = UserLogs.objects.filter(user=usr).values('action').annotate(total=Count('action')).order_by('total')
+			xdata = []
+			ydata = []
+			for obj in objs:
+				xdata.append(str(obj['action']))
+				ydata.append(str(obj['total']))		
+			#print str(xdata)
+			#print str(ydata)
+			color_list = ['#5d8aa8', '#e32636', '#efdecd', '#ffbf00', '#ff033e', '#a4c639',
+					'#b2beb5', '#8db600', '#7fffd4', '#ff007f', '#ff55a3', '#5f9ea0']
+			extra_serie = {
+				"tooltip": {"y_start": " event called ", "y_end": " times"},
+				"color_list": color_list
+				}
+			chartdata = {'x': xdata, 'y1': ydata, 'extra1': extra_serie}
+			charttype = "pieChart"
+			data1 = {
+				'charttype': charttype,
+				'chartdata': chartdata,
+				'chartcontainer': "piechart_container",
+				'extra': {
+				'x_is_date': False,
+				'x_axis_format': '',
+				'tag_script_js': True,
+				'jquery_on_ready': False,
+				}
+				}
 
-		objs_all = UserLogs.objects.all().values('action').annotate(total=Count('action')).order_by('total')		
-		xdata1 = []
-		ydata1 = []
-		for obj in objs_all:
-			xdata1.append(str(obj['action']))
-			ydata1.append(str(obj['total']))		
-		chartdata1 = {'x': xdata1, 'y1': ydata1, 'extra1': extra_serie}
-		charttype1 = "pieChart"
+			objs_all = UserLogs.objects.all().values('action').annotate(total=Count('action')).order_by('total')		
+			xdata1 = []
+			ydata1 = []
+			for obj in objs_all:
+				xdata1.append(str(obj['action']))
+				ydata1.append(str(obj['total']))		
+			chartdata1 = {'x': xdata1, 'y1': ydata1, 'extra1': extra_serie}
+			charttype1 = "pieChart"
 
-		data['charttype1'] = charttype1
-		data['chartdata1'] = chartdata1
-		data['chartcontainer1'] = "piechart_container1"
-		data['extra1'] = deepcopy(data['extra'])
+			data1['charttype1'] = charttype1
+			data1['chartdata1'] = chartdata1
+			data1['chartcontainer1'] = "piechart_container1"
+			data1['extra1'] = deepcopy(data1['extra'])
 
-		response = render_to_response('templates/analytics.html', data,
-							  context_instance=RequestContext(request))
-		return response    
+			# response = render_to_response('templates/analytics.html', data1,
+			# 					  context_instance=RequestContext(request))
+			# return response
+		# else:
+			dataSource = {}
+			dataSource['chart'] = { 
+				"caption": "User Statistics",
+				"subCaption": "Your Activity",
+				"xAxisName": "Activity Type",
+				"yAxisName": "Count",
+				"paletteColors" : "#0075c2",
+				"bgColor" : "#ffffff",
+				"borderAlpha": "20",
+				"canvasBorderAlpha": "0",
+				"usePlotGradientColor": "0",
+				"plotBorderAlpha": "10",
+				"showXAxisLine": "1",
+				"xAxisLineColor" : "#999999",
+				"showValues" : "0",
+				"divlineColor" : "#999999",
+				"divLineIsDashed" : "1",
+				"showAlternateHGridColor" : "0",
+				"showValues": "0",
+				"theme": "zune"
+				}
+
+			dataSource['data'] = []
+			dataSource['linkeddata'] = []
+
+			user = request.user.username
+			usr = SiteUser.objects.filter(username=user)[0]
+			obj = UserLogs.objects.filter(user=usr).values_list('obj')
+			obj_c = obj.distinct().count()
+			link = UserLogs.objects.filter(user=usr).values_list('link')
+			link_c = link.distinct().count()
+			main_link = UserLogs.objects.filter(user=usr).values_list('main_link')
+			main_link_c = main_link.distinct().count()
+			action = UserLogs.objects.filter(user=usr).values_list('action')
+			action_c = action.distinct().count()
+
+			#Add one with timestamp
+			n = 0
+			temp = {"Action":action_c,"Object":obj_c,"Link":link_c,"ParentLink":main_link_c}		
+			for key, value in temp.iteritems():
+			  data = {}
+			  data['label'] = key
+			  data['value'] = value
+			  data['link'] = 'newchart-json-'+ key
+			  dataSource['data'].append(data)
+			  n += 1	
+			  linkData = {}
+			  linkData['id'] = str(key)
+			  linkedchart = {}
+			  linkedchart['chart'] = {
+				"caption" : "Detailed " + key +" Info",
+				"subCaption": "Drilldown details",
+				"xAxisName": "Activity",
+				"yAxisName": "Count",
+				"showValues": "0",
+				"theme": "zune",
+				"paletteColors" : "#0075c2",
+				"bgColor" : "#ffffff",
+				"borderAlpha": "20",
+				"canvasBorderAlpha": "0",
+				"usePlotGradientColor": "0",
+				"plotBorderAlpha": "10",
+				"showXAxisLine": "1",
+				"xAxisLineColor" : "#999999",
+				"showValues": "0",
+				"divlineColor" : "#999999",
+				"divLineIsDashed" : "1",
+				"showAlternateHGridColor" : "0"
+				}
+
+			  linkedchart['data'] = []
+			  
+			  val = ""
+			  if key == 'Action':
+				objects = UserLogs.objects.filter(user=usr).values('action').annotate(total=Count('action')).order_by('total')
+				val = "action"
+			  elif key == 'Object':
+				objects = UserLogs.objects.filter(user=usr).values('obj').annotate(total=Count('obj')).order_by('total')
+				val = "obj"
+			  elif key == 'Link':
+				objects = UserLogs.objects.filter(user=usr).values('link').annotate(total=Count('link')).order_by('total')
+				val = "link"
+			  elif key == 'ParentLink':
+				objects = UserLogs.objects.filter(user=usr).values('main_link').annotate(total=Count('main_link')).order_by('total')
+				val = "main_link"
+			  
+			  for each in objects:
+				arrDara = {}
+				if str(each[val]).strip() == '' or str(each[val]).strip() == '#':
+					each[val] = "Undefined"
+				each[val] = str(each[val]).replace("https://stackoverflow.com","")
+				cleanString = re.sub('\W+','', each[val])
+				arrDara['label'] = cleanString
+				arrDara['value'] = int(each['total'])
+				# arrDara['label'] = "Ra"
+				# arrDara['value'] = n
+				linkedchart['data'].append(arrDara)
+				print arrDara
+
+			  linkData['linkedchart'] = linkedchart
+			  dataSource['linkeddata'].append(linkData)
+
+			column2D = FusionCharts("column2D", "ex1" , "600", "350", "chart-1", "json", dataSource)
+			data1['output'] = column2D.render()
+			return render_to_response('templates/analytics.html', data1, context_instance=RequestContext(request))
+
 	except Exception as e:
 		print e
 		logger.debug(e)
 		logging.warning(e)
-		return render_to_response('templates/analytics.html', {"charttype":"error","chartdata":str(e)},
-							  context_instance=RequestContext(request))    
-
-
-@login_required(login_url='/login/')
-def analytics2(request):
-	try:
-		dataSource = {}
-		dataSource['chart'] = { 
-			"caption": "User Statistics",
-			"subCaption": "Your Activity",
-            "xAxisName": "Activity Type",
-            "yAxisName": "Count",
-		    "paletteColors" : "#0075c2",
-		    "bgColor" : "#ffffff",
-		    "borderAlpha": "20",
-		    "canvasBorderAlpha": "0",
-		    "usePlotGradientColor": "0",
-		    "plotBorderAlpha": "10",
-		    "showXAxisLine": "1",
-		    "xAxisLineColor" : "#999999",
-		    "showValues" : "0",
-		    "divlineColor" : "#999999",
-		    "divLineIsDashed" : "1",
-		    "showAlternateHGridColor" : "0",
-	        "showValues": "0",
-	        "theme": "zune"
-			}
-
-		dataSource['data'] = []
-		dataSource['linkeddata'] = []
-
-		user = request.user.username
-		usr = SiteUser.objects.filter(username=user)[0]
-		obj = UserLogs.objects.filter(user=usr).values_list('obj')
-		obj_c = obj.distinct().count()
-		link = UserLogs.objects.filter(user=usr).values_list('link')
-		link_c = link.distinct().count()
-		main_link = UserLogs.objects.filter(user=usr).values_list('main_link')
-		main_link_c = main_link.distinct().count()
-		action = UserLogs.objects.filter(user=usr).values_list('action')
-		action_c = action.distinct().count()
-
-		#Add one with timestamp
-		n = 0
-		temp = {"Action":action_c,"Object":obj_c,"Link":link_c,"ParentLink":main_link_c}		
-		for key, value in temp.iteritems():
-		  data = {}
-		  data['label'] = key
-		  data['value'] = value
-		  data['link'] = 'newchart-json-'+ key
-		  dataSource['data'].append(data)
-		  n += 1	
-		  linkData = {}
-		  linkData['id'] = str(key)
-		  linkedchart = {}
-		  linkedchart['chart'] = {
-			"caption" : "Detailed " + key +" Info",
-			"subCaption": "Drilldown details",
-            "xAxisName": "Activity",
-            "yAxisName": "Count",
-			"showValues": "0",
-			"theme": "zune",
-		    "paletteColors" : "#0075c2",
-		    "bgColor" : "#ffffff",
-		    "borderAlpha": "20",
-		    "canvasBorderAlpha": "0",
-		    "usePlotGradientColor": "0",
-		    "plotBorderAlpha": "10",
-		    "showXAxisLine": "1",
-		    "xAxisLineColor" : "#999999",
-		    "showValues": "0",
-		    "divlineColor" : "#999999",
-		    "divLineIsDashed" : "1",
-		    "showAlternateHGridColor" : "0"
-			}
-
-		  linkedchart['data'] = []
-		  
-		  val = ""
-		  if key == 'Action':
-		  	objects = UserLogs.objects.filter(user=usr).values('action').annotate(total=Count('action')).order_by('total')
-		  	val = "action"
-		  elif key == 'Object':
-		  	objects = UserLogs.objects.filter(user=usr).values('obj').annotate(total=Count('obj')).order_by('total')
-		  	val = "obj"
-		  elif key == 'Link':
-		  	objects = UserLogs.objects.filter(user=usr).values('link').annotate(total=Count('link')).order_by('total')
-		  	val = "link"
-		  elif key == 'ParentLink':
-		  	objects = UserLogs.objects.filter(user=usr).values('main_link').annotate(total=Count('main_link')).order_by('total')
-		  	val = "main_link"
-		  
-		  for each in objects:
-		  	arrDara = {}
-		  	if str(each[val]).strip() == '' or str(each[val]).strip() == '#':
-		  		each[val] = "Undefined"
-			each[val] = str(each[val]).replace("https://stackoverflow.com","")
-			cleanString = re.sub('\W+','', each[val])
-			arrDara['label'] = cleanString
-			arrDara['value'] = int(each['total'])
-			# arrDara['label'] = "Ra"
-			# arrDara['value'] = n
-			linkedchart['data'].append(arrDara)
-			print arrDara
-
-		  linkData['linkedchart'] = linkedchart
-		  dataSource['linkeddata'].append(linkData)
-
-		pie3d = FusionCharts("pie3d", "ex1" , "600", "350", "chart-1", "json", dataSource)
-		return render(request, 'templates/analytics2.html', {'output': pie3d.render()})
-	except Exception as e:
-		print e
-		#logger.debug(e)
-		#logging.warning(e)
+		
