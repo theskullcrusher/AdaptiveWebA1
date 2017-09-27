3231614600f8381f49da6018a35fbb331c3c247d
Suraj Shah <ssshah22@asu.edu>
2017-09-26 20:52:27 -0700

    writeups

--- assignment1/assignment1/surajsite/views.py
+++ assignment1/assignment1/surajsite/views.py
@@ -170,8 +170,8 @@ def analytics(request):
 		data1['output'] = charts("column3D", "chart-1", 1, request)
 		data1['output1'] = charts("column3D","chart-2", 2, request)
 
-		data1['output2'] = charts("doughnut3D", "chart-3", 3, request)
-		data1['output3'] = charts("doughnut3D","chart-4", 4, request)
+		data1['output2'] = charts("doughnut3D", "chart-3", 1, request)
+		data1['output3'] = charts("doughnut3D","chart-4", 2, request)
 
 
 		return render_to_response('templates/analytics.html', data1, context_instance=RequestContext(request))
