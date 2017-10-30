a8779dff38e2e30a581cb364d103924837c04088
Suraj Shah <ssshah22@asu.edu>
2017-10-29 18:35:44 -0700

    fger

--- assignment1/assignment1/assign3_utils/utils.py
+++ assignment1/assignment1/assign3_utils/utils.py
@@ -22,8 +22,25 @@ def utils():
 
 def create_index():
 	"""Used to create index, etc"""
-	
+	with open('data.json','r') as f:
+		es = Elasticsearch()
+		doc = json.load(f)
+		print type(doc)
+		# res = es.index(index="wikibook", doc_type='book', body=doc)
+		# print(res['created'])
+
+		# res = es.get(index="test-index", doc_type='tweet', id=1)
+		# print(res['_source'])
+
+		# es.indices.refresh(index="test-index")
+
+		# res = es.search(index="test-index", body={"query": {"match_all": {}}})
+		# print("Got %d Hits:" % res['hits']['total'])
+		# for hit in res['hits']['hits']:
+		#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
 
 
 if __name__ == "__main__":
 	utils()
+	create_index()
