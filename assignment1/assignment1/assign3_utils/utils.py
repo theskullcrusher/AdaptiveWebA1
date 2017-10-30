ab678101fd80608e2ae7394abdb65a45f993d60e
Suraj Shah <ssshah22@asu.edu>
2017-10-30 07:09:58 +0000

    running indexing utils

--- assignment1/assignment1/assign3_utils/utils.py
+++ assignment1/assignment1/assign3_utils/utils.py
@@ -13,34 +13,69 @@ from django.db.models import Count, Sum, Max, Min
 from copy import deepcopy
 import re
 from django.contrib.auth.models import User
-import json
-from elasticsearch import ElasticSearch
+import json, re
+from elasticsearch import Elasticsearch
+import pprint
 
 def utils():
 	pass
+
+def search_index(search):
+	"""Search an index"""
+        with open('data.json','r') as f:
+                es = Elasticsearch()
+                docs = json.load(f)
+#		body = {"query": {"match": {"wikibooks.content":search}}, 
+#			"_source": ["wikibooks.title","wikibooks.content", "wikibooks.url"]
+#			}
+                body = {"query": {"multi_match": {"query":search, "fields":["wikibooks.content","wikibooks.subdata.content"]}}
+#                        "_source": ["wikibooks.title","wikibooks.content", "wikibooks.url", "wikibooks."]
+                        }
+#		sub_body = {"query": {'match':{'wikibooks.subdata.content':search}}, 
+#                        "_source": ["wikibooks.subdata.content", "wikibooks.subdata.title"]
+#                        }
+		res = es.search(index='wikibooks',body=body)	
+		pp = pprint.PrettyPrinter(indent=4)
+		#pp.pprint(res)
+		return res
 
 
 def create_index():
 	"""Used to create index, etc"""
 	with open('data.json','r') as f:
 		es = Elasticsearch()
-		doc = json.load(f)
-		print type(doc)
-		# res = es.index(index="wikibook", doc_type='book', body=doc)
-		# print(res['created'])
+		docs1 = json.load(f)
+		docs = {}
+		docs['wikibooks'] = docs1
+		#print doc[0]
+		failed_count = 0
+		failed_count1 = 0
+		#es.delete(index='wikibook', doc_type='book', id=1)
+		#results = []
+#		for n, each_doc in enumerate(docs):
+		try:
+#			val = each_doc['title'].strip().replace(' ', '').lower()
+#			val = re.sub('[^0-9a-zA-Z]+', '', val)
+			es.delete(index='wikibooks', doc_type='book', id=1)
+			res = es.index(index='wikibooks', doc_type='book', body=docs, op_type='create', id=1)
+			if not res['created']:
+				failed_count += 1
+#				continue
+		except Exception as e:
+			print e
+			failed_count1 += 1
+#			continue		
 
-		# res = es.get(index="test-index", doc_type='tweet', id=1)
-		# print(res['_source'])
+		#print('Created? ',res['created'])
+		print "Failed insertions:",failed_count, ' ', failed_count1
 
-		# es.indices.refresh(index="test-index")
-
-		# res = es.search(index="test-index", body={"query": {"match_all": {}}})
-		# print("Got %d Hits:" % res['hits']['total'])
-		# for hit in res['hits']['hits']:
-		#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
-
+#		res = es.search(index="wikibook", body={"query": {"match_all": {'title':'history'}}})
+#		print("Got %d Hits:" % res['hits']['total'])
+#		for hit in res['hits']['hits']:
+#		     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
 
 
 if __name__ == "__main__":
 	utils()
 	create_index()
+	search_index("Java")
