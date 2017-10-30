1f4c9f8daccc9ca64d36e9453e0b9512b1cb2de6
Suraj Shah <ssshah22@asu.edu>
2017-10-30 15:57:22 -0700

    updated index creation

--- assignment1/assignment1/assign3_utils/utils.py
+++ assignment1/assignment1/assign3_utils/utils.py
@@ -44,32 +44,47 @@ def create_index():
 	"""Used to create index, etc"""
 	with open('data.json','r') as f:
 		es = Elasticsearch()
-		docs1 = json.load(f)
-		docs = {}
-		docs['wikibooks'] = docs1
+		docs = json.load(f)
+		# docs = {}
+		# docs['wikibooks'] = docs1
 		#print doc[0]
 		failed_count = 0
 		failed_count1 = 0
+		failed_count2 = 0
 		#es.delete(index='wikibook', doc_type='book', id=1)
 		#results = []
+		id_count = 1
 		for n, each_doc in enumerate(docs):
 			try:
 				# val = each_doc['title'].strip().replace(' ', '').lower()
 				# val = re.sub('[^0-9a-zA-Z]+', '', val)
-				es.delete(index='wikibooks', doc_type='book', id=1)
-				exit()
-				es.delete(index='wikibooks', doc_type='book', id=n+1)
-				res = es.index(index='wikibooks', doc_type='book', body=docs, op_type='create', id=1)
+				# es.delete(index='wikibooks', doc_type='book', id=1)
+				# exit()
+				# es.delete(index='wikibooks', doc_type='book', id=id_count)
+				each_doc['flag'] = True
+				url = each_doc['url']
+				subdata = each_doc['subdata']
+				del each_doc['subdata']
+				res = es.index(index='wikibooks', doc_type='book', body=each_doc, op_type='create', id=id_count)
+				id_count+=1
 				if not res['created']:
 					failed_count += 1
-					continue
+
+				for d in subdata:
+					d['url'] = url
+					d['flag'] = False
+					res = es.index(index='wikibooks', doc_type='book', body=d, op_type='create', id=id_count)
+					id_count+=1
+					if not res['created']:
+						failed_count1 += 1		
+
 			except Exception as e:
 				print e
-				failed_count1 += 1
+				failed_count2 += 1
 				continue		
 
 		#print('Created? ',res['created'])
-		print "Failed insertions:",failed_count, ' ', failed_count1
+		print "Failed insertions:",failed_count, ' ', failed_count1, ' ', failed_count2
 
 #		res = es.search(index="wikibook", body={"query": {"match_all": {'title':'history'}}})
 #		print("Got %d Hits:" % res['hits']['total'])
@@ -80,4 +95,4 @@ def create_index():
 if __name__ == "__main__":
 	utils()
 	create_index()
-	search_index("Java")
+	#search_index("Java")
